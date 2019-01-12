7a0e4054968bc78fdce774ca538f50482427b618
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2019-01-11 22:29:43 -0300

    simple test

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import BooksApp from './components/BooksApp';
 
+console.log('testtttttttttttttttt working....');
 ReactDOM.render(
   <BrowserRouter><BooksApp /></BrowserRouter>,
   document.getElementById('root'),
