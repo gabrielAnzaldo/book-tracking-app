a624539a4aaf28977af2bfd24b49a5193549b095
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2019-01-16 21:22:59 -0300

    Updates

---
+++ precache-manifest.6fc510c1757522516269859b563f3ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/book-tracking-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "122e707b0d982ccfb8d9",
+    "url": "/book-tracking-app/static/js/runtime~main.122e707b.js"
+  },
+  {
+    "revision": "3da27fe8064655b3096a",
+    "url": "/book-tracking-app/static/js/main.3da27fe8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/book-tracking-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3da27fe8064655b3096a",
+    "url": "/book-tracking-app/static/css/main.56e9ec49.chunk.css"
+  },
+  {
+    "revision": "54369e5c259bcd20d895c92a097f99b8",
+    "url": "/book-tracking-app/index.html"
+  }
+];
