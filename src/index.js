0dd760cc29e2a2c966e0beb793b8b4c6e6f3bad7
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2019-01-26 17:04:46 -0300

    Abb basename

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import BooksApp from './components/BooksApp';
 
 ReactDOM.render(
-  <BrowserRouter basename="/"><BooksApp /></BrowserRouter>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BooksApp />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
